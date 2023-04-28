d1d30a461ae48d01d0de5ecb0cf37121b35dd27a
Eren Kaya <115587114+19soft1096@users.noreply.github.com>
2023-04-28 17:58:27 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -21,6 +21,7 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.message').textContent = 'Correct Number..!!!';
       document.querySelector('body').style.backgroundColor = '#60b347';
       document.querySelector('.number').style.width = '30rem';
+      document.querySelector('.number').textContent = secretNumber;
     }
   } else if (guess > secretNumber) {
     if (score > 0) {
